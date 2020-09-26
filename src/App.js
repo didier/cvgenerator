0491af0324cc04b0e8888e91564de8a1832c31c4
Robin de Laater <robindelaater@Robins-MacBook-Pro.local>
2020-09-26 12:12:53 +0200

    Fix routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useParams } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <InputForm />
-        </Route>
-        <Route path="/output">
-          <GeneratedOutput />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/output">
+            <GeneratedOutput />
+          </Route>
+          <Route path="/">
+            <InputForm />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
@@ -27,28 +29,33 @@ function InputForm() {
   }
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit} id="input-form">
-        <h2>Enter details to generate your CV.</h2>
-        <label htmlFor="name">
-          Full name:
+    <form action="/output" id="input-form">
+      <h2>Enter details to generate your CV.</h2>
+      <label htmlFor="name">
+        Full name:
            <input type="text" name="name" onChange={(event) => setName(event.target.name)} />
-        </label>
-        <label htmlFor="email">
-          Email address:
+      </label>
+      <label htmlFor="email">
+        Email address:
            <input type="text" name="email" onChange={(event) => setEmail(event.target.email)} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   )
 }
 
 function GeneratedOutput() {
+  const urlParams = new URLSearchParams(window.location.search)
+
+  let fullname = urlParams.get('name')
+  let email = urlParams.get('email')
+
   return (
     <div id="output">
       <h2>Your CV:</h2>
       <div className="generated-cv">
+        <h2>Full name: {fullname}</h2>
+        <p>Email Address: {email}</p>
       </div>
     </div>
   )
